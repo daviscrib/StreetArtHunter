46b2a4f4d2dfc2f15b54e4289531e546e044bc91
David <david@scrib-art.fr>
2024-08-25 00:56:26 +0200

    Fix test connexion

--- server/tests/connexion/connexion.spec.js
+++ server/tests/connexion/connexion.spec.js
@@ -1,23 +1,19 @@
 const { app, request } = require("../config");
 
-describe('Test de connexion utilisateur', () => {
-  it('doit répondre avec un statut HTTP 200 pour une connexion réussie', async () => {
-    const response = await request(app)
-      .post('/api/auths/login') 
-      .send({
-        pseudo: 'Dav', // Remplacez par un nom d'utilisateur valide
-        password: 'david' // Remplacez par un mot de passe valide
-      });
+describe("Test de connexion utilisateur", () => {
+  it("doit répondre avec un statut HTTP 200 pour une connexion réussie", async () => {
+    const response = await request(app).post("/api/auths/login").send({
+      pseudo: "david", // Remplacez par un nom d'utilisateur valide
+      password: "userdavid123", // Remplacez par un mot de passe valide
+    });
     expect(response.status).toBe(200);
   });
 
-  it('doit répondre avec un statut HTTP 404 pour une connexion échouée', async () => {
-    const response = await request(app)
-      .post('/api/auths/login') 
-      .send({
-        pseudo: 'wronguser', 
-        password: 'wrongpassword' 
-      });
-    expect(response.status).toBe(404);
+  it("doit répondre avec un statut HTTP 404 pour une connexion échouée", async () => {
+    const response = await request(app).post("/api/auths/login").send({
+      pseudo: "wronguser",
+      password: "wrongpassword",
+    });
+    expect(response.status).toBe(422);
   });
 });
